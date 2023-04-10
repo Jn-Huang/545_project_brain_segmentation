24c9871caee991d1b0eca83ea1e9689b7810876a
AnthonySun1300 <123266336+AnthonySun1300@users.noreply.github.com>
2023-04-09 22:53:14 -0400

    segformer model

--- models/segformer.py
+++ models/segformer.py
@@ -4,22 +4,23 @@ from transformers import SegformerConfig, SegformerModel, SegformerDecodeHead
 from transformers import SegformerFeatureExtractor as SegformerImageProcessor
 
 class CustomSegModel(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config= SegformerConfig.from_pretrained("nvidia/segformer-b5-finetuned-cityscapes-1024-1024")):
         super(CustomSegModel, self).__init__()
         self.config = config
-        if config.from_zoo is not None:
-            self.processor = SegformerImageProcessor.from_pretrained(config.from_zoo)
-            self.model = SegformerModel.from_pretrained(config.from_zoo)
-            self.decode_head = SegformerDecodeHead.from_pretrained(config.from_zoo)
-            self.decode_head.classifier = torch.nn.Conv2d(
-                self.decode_head.config.decoder_hidden_size,
-                config.num_labels,
-                kernel_size=1
-            )
-        else:
-            self.processor = SegformerImageProcessor(config)
-            self.model = SegformerModel(config)
-            self.decode_head = SegformerDecodeHead(config)
+        # if config.from_zoo is not None:
+        #     self.processor = SegformerImageProcessor.from_pretrained(config.from_zoo)
+        #     self.model = SegformerModel.from_pretrained(config.from_zoo)
+        #     self.decode_head = SegformerDecodeHead.from_pretrained(config.from_zoo)
+        #     self.decode_head.classifier = torch.nn.Conv2d(
+        #         self.decode_head.config.decoder_hidden_size,
+        #         config.num_labels,
+        #         kernel_size=1
+        #     )
+        # else:
+        
+        self.processor = SegformerImageProcessor(config)
+        self.model = SegformerModel(config)
+        self.decode_head = SegformerDecodeHead(config)
     
     def forward(self, images):
         inputs = self.processor(images, return_tensors="pt")
